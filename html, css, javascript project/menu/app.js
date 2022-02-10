a7ef9e8958c00ec24a30cfa928c53af1ffa8b75c
huimin <huim.cao@gmail.com>
2022-02-10 13:34:43 +0100

    Finish this

--- html, css, javascript project/menu/app.js
+++ html, css, javascript project/menu/app.js
@@ -71,4 +71,78 @@ const menu = [
     img: "./images/item-9.jpeg",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
-];
+]
+
+const sectionCenter = document.querySelector(".section-center")
+const btnContainer = document.querySelector(".btn-container")
+
+
+// Das DOMContentLoaded -Event wird ausgelöst, wenn das initiale HTML-Dokument vollständig geladen und geparst ist.
+window.addEventListener("DOMContentLoaded", function() {
+
+    displayMenuItems(menu)
+    displayMenuButtons()
+
+})
+
+
+// render the page
+function displayMenuItems(menuItems){
+  let displayMenu = menuItems.map(function(item){
+    return  `<article class="menu-item">
+    <img src="${item.img}" alt=${item.title} class="photo">
+    <div class="item-info">
+      <header> 
+      <h4>${item.title}</h4>
+      <h4 class="price">$${item.price}</h4>
+      </header>
+      <p class="item-text">${item.desc}</p>
+    </div>
+     </article>`
+  }) 
+
+    displayMenu = displayMenu.join("")
+    sectionCenter.innerHTML = displayMenu
+}
+
+
+function displayMenuButtons(){
+    // get the unique category
+    const category=menu.reduce(function(values, item){
+        if(!values.includes(item.category)){
+            values.push(item.category)
+        }
+        return values
+    }, ['all'])
+
+    // render the unique category buttons in the page
+    const categoryBtns = category
+                        .map(function(item){
+                            return `<button class="filter-btn" type="button" data-id=${item}>${item}</button>`
+                        }).join("")
+
+    btnContainer.innerHTML = categoryBtns;
+    const filterBtns = btnContainer.querySelectorAll(".filter-btn");
+
+   
+
+    // const filterBtns = btnContainer.querySelectorAll(".filter-btn")
+    console.log(filterBtns)
+    console.log(btnContainer)
+
+    // render the page according to the buttton that is clicked
+    filterBtns.forEach(function(btn){
+        btn.addEventListener("click", function(e){
+            const menuCatetgory = menu.filter(function(menuItem){
+                // console.log(e.currentTarget.dataset);
+                if(menuItem.category === e.currentTarget.dataset.id) 
+                return menuItem
+            })
+            if(category === "all"){
+                displayMenuItems(menu)
+            }else{
+                displayMenuItems(menuCatetgory)
+            }
+        })
+    })
+}

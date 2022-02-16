21bf416a7578ffbda75df418136b4042ee13e2a4
huimin <huim.cao@gmail.com>
2022-02-16 15:24:54 +0100

    update

--- React Project/redux_calc/src/redux/constants.js
+++
@@ -1,2 +0,0 @@
-const INCREMENT='increment'
-const DECREMENT='decrement'
--- React Project/redux_calc/src/redux/count_action.js
+++
@@ -1,5 +0,0 @@
-import {INCREMENT, DECREMENT} from './constants'
-
-
-export const createIncrementAction = (data) => ({type:INCREMENT, data: data})
-export const createDecrementAction = (data) => ({type:DECREMENT, data: data})
